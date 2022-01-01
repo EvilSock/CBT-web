3c60156c0ca92764a3dd970a3730cf73a759628c
EvilSock <paulrossouw11@gmail.com>
2022-01-01 10:26:13 +0200

    fixed issues with ie compatability

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,6 @@
 
 
-$(document).ready(()=>{
+$(document).ready(function(){
     $(".float-in-r").css({"margin-left":"0","opacity":"100%"});
     $(".float-in-l").css({"margin-left":"0","opacity":"100%"});
 });
