2997c5d2c8579c36bffd53e4737755df5d71c52b
EvilSock <paulrossouw11@gmail.com>
2022-02-09 20:50:32 +0200

    we did a lot, basically implemented collapsoble

--- javascript/recent-courses.js
+++ javascript/recent-courses.js
@@ -1,15 +1,45 @@
 
+
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
+    
     this.classList.toggle("active");
     var content = this.nextElementSibling;
     if (content.style.maxHeight){
       content.style.maxHeight = null;
     } else {
       content.style.maxHeight = content.scrollHeight + "px";
+      
     } 
   });
 }
+
+$(".collapsible-jaco").click(function(){
+  if ($("#fade-1").html() == "click for more"){
+    $("#fade-1").html("click for less");
+  } else {
+    $("#fade-1").html("click for more");
+  }
+  $("#fade-2").toggleClass("fade-2-flip");
+  $("#fade-3").toggleClass("fade-3-flip");
+});
+
+$(".collapsible-bradley").click(function(){
+  if ($("#fade-4").html() == "click for more"){
+    $("#fade-4").html("click for less");
+  } else {
+    $("#fade-4").html("click for more");
+  }
+  $("#fade-5").toggleClass("fade-2-flip");
+  $("#fade-6").toggleClass("fade-3-flip");
+});
+
+
+
+    
+
+    
+    
